49d755c0e048b1308ceba80c3d0a914f0633d8f8
ritchey0713 <ritchey0713@gmail.com>
2018-08-14 20:42:21 -0400

    add footer/nav bar

--- photo-app-client/src/containers/Posts.js
+++ photo-app-client/src/containers/Posts.js
@@ -2,31 +2,22 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux';
 
+import PostList from '../components/PostList'
+import Navbar from './static/Navbar'
+import Footer from './static/Footer'
+import JumboNewPost from './static/JumboNewPost'
+
 const Posts = ({match, posts}) => {
+  <div>
+    <NavBar />
+    <Jumbotron title='Current Posts' subtitle="Click to read more!" />
+      <PostList posts={posts} />
+      <Switch>
+        <Route path={`${match.url}/postId`} component={PostShow} />
+      </Switch>
 
-// make a post list component for this
-//   const renderPosts = posts.map((post, index) => {
-//     <span key={post.title}>{post.title} </span>
-//   })
-//
-//   return(
-//     <ul>
-//     {renderPosts}
-//     </ul>
-//   )
-// }
-
-
-
-<div>
-  <PostList posts={posts} />
-  <Switch>
-  <Route path={`${match.url}/new`} component={NewBlogPost} />
-  <Route path={`${match.url}/postId`} component={PostShow} />
-</Switch>
-
-
-</div>
+      <Footer />
+  </div>
 }
 
 
